1c796f0d07d20ba22e75b68e8a5fbcc779697e52
xuning <xuningnh@qq.com>
2022-09-22 18:56:14 +0800

    camunda 学习 外部html表单他妈的为啥不行

--- my-project/src/main/java/com/example/workflow/Application.java
+++ my-project/src/main/java/com/example/workflow/Application.java
@@ -4,7 +4,7 @@ import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.example.*"})
+@SpringBootApplication
 @EnableProcessApplication
 public class Application {
 
